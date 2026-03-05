878fff88efd23c6ca878202d33c1c25f7710e971
Yogapriyan07 <yogapriyan74@gmail.com>
2026-03-05 09:48:26 +0530

    welcome palindrome

---
+++ src/PalindromeCheckerApp/PalindromeCheckerApp.java
@@ -0,0 +1,88 @@
+package PalindromeCheckerApp;
+
+    public class PalindromeCheckerApp {
+
+        public static void main(String[] args) {
+
+            String input = "madam";   // Example input
+            String reversed = "";
+
+            // Iterate from the last character to the first
+            for (int i = input.length() - 1; i >= 0; i--) {
+                reversed += input.charAt(i);
+            }
+
+            // Compare original and reversed strings
+            if (input.equals(reversed)) {
+                System.out.println(input + " is a palindrome.");
+            } else {
+                System.out.println(input + " is not a palindrome.");
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/PalindromeCheckerApp1/PalindromeCheckerApp.java
@@ -0,0 +1,24 @@
+package PalindromeCheckerApp1;
+
+public class PalindromeCheckerApp {
+
+
+    public static void main(String[] args) {
+
+        String input = "madam";   // Example input
+        String reversed = "";
+
+        // Iterate from the last character to the first
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        // Compare original and reversed strings
+        if (input.equals(reversed)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+}
+
